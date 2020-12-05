c55a429e9541e5a6f361b46c09905c47bedca5a9
Junoh Moon <mjo970625@gmail.com>
2020-12-05 14:19:46 +0900

    Support sync feature

--- KotlinSixtyFive/src/main/kotlin/kr/ac/kaist/ecl/mjo/Sixtyfive.kt
+++ KotlinSixtyFive/src/main/kotlin/kr/ac/kaist/ecl/mjo/Sixtyfive.kt
@@ -2,16 +2,14 @@ package kr.ac.kaist.ecl.mjo
 
 import com.dropbox.core.DbxRequestConfig
 import com.dropbox.core.v2.DbxClientV2
+import com.dropbox.core.v2.files.FileMetadata
 import com.dropbox.core.v2.files.WriteMode
 import kotlinx.serialization.decodeFromString
 import kotlinx.serialization.encodeToString
 import kotlinx.serialization.json.Json
 import kr.ac.kaist.ecl.mjo.Zip.pack
 import kr.ac.kaist.ecl.mjo.Zip.unpack
-import java.io.BufferedReader
-import java.io.ByteArrayOutputStream
-import java.io.InputStream
-import java.io.InputStreamReader
+import java.io.*
 import java.nio.file.Paths
 
 class Sixtyfive(configName: String = "configs.json") {
@@ -50,7 +48,7 @@ class Sixtyfive(configName: String = "configs.json") {
 
 		envs["STEAM"] = steamDirectory
 		watchList.forEach { watchDog.register(it) { _ -> backup(it) } }
-		//watchList.forEach(this::sync)
+		watchList.parallelStream().forEach(this::sync)
 	}
 
 	val String.toZipName: String
@@ -59,23 +57,48 @@ class Sixtyfive(configName: String = "configs.json") {
 		get() = "/data/${this.toZipName}"
 
 	fun watchProcesses() = watchDog.start()
-	private fun sync(processName: String): Nothing = TODO()
+
+	private fun sync(processName: String) {
+		val (stream, remoteTime) = downloadZip(processName)
+			.let { it.first to it.second?.serverModified?.time }
+
+		val localTime = config
+			.applications
+			.firstOrNull { it.name == processName }
+			?.savePath
+			?.let(::File)
+			?.lastModified()
+
+		when (remoteTime?.let { localTime?.compareTo(it) }) {
+			in Int.MIN_VALUE until 0 -> stream?.let { unpackToDestination(processName, it) }
+			in 1 until Int.MAX_VALUE -> backup(processName)
+			0 -> println("$processName: is identical")
+			//null -> println("$processName got a problem")
+		}
+	}
 
 	fun restore(processName: String) {
-		val stream = ByteArrayOutputStream()
-			.also { dropbox.files().downloadBuilder(processName.toUploadZipName).download(it) }
-			.toByteArray()
-			.inputStream()
-
-		val savePath = config
-			.applications
-			.first { it.name == processName }
-			.savePath
-			.let(this::expandPath)
-
-		unpack(stream, savePath)
-		println("$processName is successfully restored")
+		downloadZip(processName)
+			.first
+			?.let { unpackToDestination(processName, it) }
 	}
+
+	private fun downloadZip(processName: String): Pair<ByteArrayInputStream?, FileMetadata?> = ByteArrayOutputStream()
+		.runCatching { this to dropbox.files().downloadBuilder(processName.toUploadZipName).download(this) }
+		.map { it.first.toByteArray().inputStream() to it.second }
+		.getOrElse {
+			println("Failed to download $processName from dropbox")
+			null to null
+		}
+
+	private fun unpackToDestination(processName: String, stream: InputStream) = config
+		.applications
+		.firstOrNull { it.name == processName }
+		?.savePath
+		?.let(this::expandPath)
+		?.also { unpack(stream, it) }
+		?.run { println("$processName is successfully restored") }
+		?: println("$processName does not exists")
 
 	fun backup(processName: String) {
 		println("$processName has terminated")
